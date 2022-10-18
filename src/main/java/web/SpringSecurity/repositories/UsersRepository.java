25a810bf454d8006597cbc5f09cfb6258c0bc22d
Maria Filatova <m.bronskaya@gmail.com>
2022-10-18 16:59:38 +0300

    добавила запрос для подгрузки таблицы к методу

--- src/main/java/web/SpringSecurity/repositories/UsersRepository.java
+++ src/main/java/web/SpringSecurity/repositories/UsersRepository.java
@@ -1,11 +1,14 @@
 package web.SpringSecurity.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import web.SpringSecurity.model.User;
 
 
 @Repository
 public interface UsersRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    @Query("select c from User c join fetch c.roles where c.username = :username")
+    User findByUsername(@Param("username") String username);
 }
